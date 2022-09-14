f0e8d68f995fb6e65bbadd08b21452c82e03dce9
Abhijeet-TruckX <abhijeet@truckx.com>
2022-09-14 23:04:49 +0530

    Project restructured

---
+++ src/test/java/project/testscenarios/LoginScript.java
@@ -0,0 +1,74 @@
+package project.testscenarios;
+
+import java.io.IOException;
+
+import org.apache.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import project.utility.*;
+
+public class LoginScript extends ConfigurationSetup {
+		
+	WebPageObjectCreation repo;
+	
+	WebDriver driver;
+	
+	public static Logger log = Logger.getLogger(LoginScript.class);
+	@Test(priority = 0)
+	public void ConfigurationSetup() throws InterruptedException, IOException
+	{
+		Driver.SetDriver(getDriverInstance());
+			
+		driver=Driver.GetDriver();
+			
+		ObjectRepository.SetInstance(new WebPageObjectCreation(driver));
+			
+		repo = ObjectRepository.GetInstance();
+	}
+	
+	@Test(priority = 1)
+	public void blankUserNameandPassword() throws InterruptedException
+	{
+		repo.loginPageObject().enterUserName("");
+		
+		repo.loginPageObject().enterUserPassword("");
+		
+		repo.loginPageObject().signIn();
+		
+		Assert.assertTrue(repo.loginPageObject().validate());
+		
+		refreshPage();
+		
+	}
+	
+	@Test(priority = 2,alwaysRun = true)
+	public void inValidUserNameandPassword() throws InterruptedException
+	{
+		repo.loginPageObject().enterUserName("Abhijeet@gmail.com");
+		
+		repo.loginPageObject().enterUserPassword("Abhijeet123");
+		
+		repo.loginPageObject().signIn();
+		
+		Assert.assertTrue(repo.loginPageObject().validate());
+				
+		refreshPage();
+		
+	}
+	
+	@Test(priority = 3,alwaysRun = true)
+	public void validUserNameandPassword() throws InterruptedException
+	{
+	
+		repo.loginPageObject().enterUserName();
+		
+		repo.loginPageObject().enterUserPassword();
+		
+		repo.loginPageObject().signIn();
+		
+		Assert.assertTrue(repo.loginPageObject().validate());
+	
+	}
+}
